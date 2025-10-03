089385094d621832eb63fd1f30a106c34568a9ee
SHAISTA AFREEN <165583874+SHAISTTAA@users.noreply.github.com>
2025-10-03 20:41:49 +0000

    Update second.java

---
+++ Array/second.java
@@ -0,0 +1,9 @@
+public class second {
+    public static void main(String[] args) {
+        String names[] = { "Peter", "Parker", "William", "Shubham", "Shreya" };
+
+        for (String name : names) {
+            System.out.println("Hellow! My name is : "+ name);
+        }
+    }
+}
