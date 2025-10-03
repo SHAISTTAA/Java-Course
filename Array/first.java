442cc24a986f01db467107781a959875be2d4437
SHAISTA AFREEN <165583874+SHAISTTAA@users.noreply.github.com>
2025-10-03 13:20:59 +0000

    Fix syntax errors in first.java

---
+++ Array/first.java
@@ -0,0 +1,9 @@
+public class first {
+    public static void main(String[] args) {
+        String[] name = {"shaista", "rohan", "abhi", "manya"};
+
+        for (int i = 0; i < name.length; i++) {
+            System.out.println("The name is : " + name[i]);
+        }
+    }
+}
