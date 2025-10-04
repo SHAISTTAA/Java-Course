673ee8f839804c7ccfb568dda35cb8bd55489d8f
SHAISTA AFREEN <165583874+SHAISTTAA@users.noreply.github.com>
2025-10-04 11:07:51 +0000

    Fix foreachloop.java: correct variable name and loop syntax

---
+++ foreachloop.java
@@ -0,0 +1,14 @@
+public class foreachloop {
+    public static void main(String args []){
+        int age [] = {12, 13, 14, 15, 16};
+
+        for (int eachage : age){
+            System.out.println("The age is : " + eachage);
+        }
+        String name[] = {"John", "Jane", "Doe"};
+        for (String n : name) {
+            System.out.println("The name is : " + n);
+        }
+     }
+    
+}
