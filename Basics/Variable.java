54edc538c06904d38385c917b1d3516a492d94e0
SHAISTA AFREEN <165583874+SHAISTTAA@users.noreply.github.com>
2025-10-04 11:54:52 +0000

    Add Basics/Variable.java to Basics folder

---
+++ Basics/Variable.java
@@ -0,0 +1,11 @@
+package Basics;
+
+public class Variable {
+    public static void main(String args[]){ 
+        int num = 10;
+        int age = 25;
+        System.out.println("The number is : " + num);
+        System.out.println("The age is : " + age);
+    
+}
+}
