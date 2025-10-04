c50a6fd15292e42d4411796135225c442f21bcb4
SHAISTA AFREEN <165583874+SHAISTTAA@users.noreply.github.com>
2025-10-04 11:22:08 +0000

    Add Basics/DataTypes.java in new Basics folder

---
+++ Basics/DataTypes.java
@@ -0,0 +1,33 @@
+package Basics;
+
+public class DataTypes {
+    public static void main(String args[]){
+        byte Number = 100;
+        System.out.println("The byte value is : " + Number);
+    
+    boolean prdecise = true;
+    System.out.println("The boolean value is : " + prdecise);
+
+    char Alphabet = 'A';
+    System.out.println("The char value is : " + Alphabet);
+
+    int age = 22;
+    System.out.println("The int value is : " + age);
+
+    short Student = 1300;
+    System.out.println("The short value is : " + Student);
+
+    float pi = 3.14f;
+    System.out.println("The float value is : " + pi);
+
+    double largeDecimal = 3.141592653589793;
+    System.out.println("The double value is : " + largeDecimal);
+
+    long largeInteger = 123456789;
+    System.out.println("The long value is : " + largeInteger);
+
+        String name = "John Doe";
+        System.out.println("The string value is : " + name);
+}
+    
+}
