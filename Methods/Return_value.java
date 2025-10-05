75d329b804e43101eaef883a71c0dd743dd50d34
SHAISTA AFREEN <165583874+SHAISTTAA@users.noreply.github.com>
2025-10-05 11:00:35 +0000

    Fix Methods/Return_value.java: correct method parameter and return value

---
+++ Methods/Return_value.java
@@ -0,0 +1,11 @@
+package Methods;
+
+public class Return_value {
+    static int number(int number){
+        return number;
+    }
+    public static void main(String args[]){
+        System.out.println(number(1234567890));
+    }
+    
+}
