abebd50c29a8afb05ee43a72e6952ae511534dc7
SHAISTA AFREEN <165583874+SHAISTTAA@users.noreply.github.com>
2025-10-06 12:43:20 +0000

    Fix and update TernaryOperator.java in Operators folder

---
+++ Operators/TernaryOperator.java
@@ -0,0 +1,8 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+        int a = 10, b = 20, max;
+        
+        max = (a > b) ? a : b;
+    System.out.println("Max of " + a + " and " + b + " is " + max);
+    }
+}
