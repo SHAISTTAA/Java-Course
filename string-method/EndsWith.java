a9aba94441d8688f86614b693662e4271605791e
SHAISTA AFREEN <165583874+SHAISTTAA@users.noreply.github.com>
2025-10-08 01:11:25 +0530

    Add files via upload

---
+++ string-method/EndsWith.java
@@ -0,0 +1,7 @@
+public class EndsWith {
+    public static void main(String[] args) {
+        String string = "Shaista Afreen";
+
+        System.out.println(string.endsWith("reen"));
+    }
+}
---
+++ string-method/StartsWith.java
@@ -0,0 +1,9 @@
+
+public class StartsWith {
+    public static void main(String[] args) {
+        String string = "Shaista Affreen";
+
+        System.out.println(string.startsWith("Shais"));
+    }
+}
+        
